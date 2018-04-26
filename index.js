6c704f727a3f76d75ac03b46fb5fe6a0cfe9c92e
awn <awn@turnlunchon.com>
2018-04-26 19:51:49 +0400

    merge commit
    commit 1 to rebase
    
    commit 2 to rebase

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { AppRegistry } from 'react-native';
+commit 1
 import App from './App';
 
 import MyApp from './MyApp';
-
+commit 2
 import PushService from './PushService';
 
 PushService.configure();
