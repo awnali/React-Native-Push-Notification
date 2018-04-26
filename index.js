cfc2e4f42cf3c4fcedc23145ca386b6bb133598a
awn <awn@turnlunchon.com>
2018-04-26 16:03:05 +0400

    commit 1 to rebase

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-
 import MyApp from './MyApp';
 import PushService from './PushService';
 
 PushService.configure();
-
-
 AppRegistry.registerComponent('PushSNSTest', () => MyApp);
